cf2fb973599b08843816c52cfbb6743507844930
User1-today <79199700+User1-today@users.noreply.github.com>
2021-02-17 16:41:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function one(){
+  window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe")
+}
